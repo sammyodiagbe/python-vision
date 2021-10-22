7d448bcdf80619344fa7996b556069c4de63ce5e
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-10-22 16:06:16 +0100

    understanding class methods in python

--- python classes and objects/classes.py
+++ python classes and objects/classes.py
@@ -6,7 +6,10 @@ class Animal:
         self.name = name
         self.category = category
 
-
+    def say_something(self,something):
+        string = 'You said {}'
+        print(string.format(something))
 dog = Animal('Johana', 'Sub category')
 print(dog.name)
 print(dog.category)
+dog.say_something('woof woof')
